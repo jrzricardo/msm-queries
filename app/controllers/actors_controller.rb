84d95ff4d92c6525c24d744259b756f5f0878293
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-15 14:19:45 +0000

    adds actor#show

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,14 @@ class ActorsController < ApplicationController
   def index
     render({ :template => "actor_templates/list"})
   end
+
+  def show
+    actor_id = params.fetch("actor_id")
+    
+    matching_records = Actor.where({ :id => actor_id})
+    
+    @the_actor = matching_records.at(0)
+
+    render({ :template => "actor_templates/details"})
+  end
 end
