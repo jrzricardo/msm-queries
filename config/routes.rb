64db256f7e6daf6ca8f54568227a351cfb6a5445
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-15 03:28:30 +0000

    directors page built

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
-  get("/directors", {:controller => "directors" :action => "director_list"})
+  get("/directors", { :controller => "directors", :action => "index" })
 
-  get("/directors/:director_id", {:controller => "directors" :action => "director_details" })
+  get("/directors/:director_id", { :controller => "directors", :action => "director_details" })
 
-  get("/directors/youngest", {:controller => "directors" :action => "youngest_director"})
+  get("/directors/youngest", { :controller => "directors", :action => "youngest_director" })
 
-  get("directors/eldest", {:controller => "directors" :action => "eldest_director"})
+  get("directors/eldest", { :controller => "directors", :action => "eldest_director" })
 
-  get("/movies", {:controller => "movies" :action => "movie_list"})
+  get("/movies", { :controller => "movies", :action => "index" })
 
-  get("/movies/movie_id" {:controller => "movies" :action => "movie_details"})
+  get("/movies/:movie_id", { :controller => "movies", :action => "movie_details" })
 
-  get("/actors", {:controller => "actors" :action => "actor_list"})
+  get("/actors", { :controller => "actors", :action => "index" })
 
-  get("/actors/:actor_id", {:controller => "actors" :action => "actor_details"})
+  get("/actors/:actor_id", { :controller => "actors", :action => "actor_details" })
 
 end
